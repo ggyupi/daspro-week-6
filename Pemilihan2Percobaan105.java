73d4d10454cc2fadf0745f033e15b7cdc3924ab5
ggyupi <beryl32451@gmail.com>
2023-10-02 12:17:28 +0700

    first commit

---
+++ Pemilihan2Percobaan105.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/**
+ * Pemilihan2Percobaan105
+ */
+public class Pemilihan2Percobaan105 {
+
+    public static void main(String[] args) {
+        Scanner input05 = new Scanner(System.in);
+        System.out.print("Masukkan tahun : ");
+        int tahun = input05.nextInt();
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) 
+                System.out.println("Tahun Kabisat");   
+        } else 
+            System.out.println("Bukan Tahun Kabisat");
+        input05.close();
+    }
+}
