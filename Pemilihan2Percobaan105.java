2a2a034984f545cb0d42a9da1837cc5ddfa22f6e
ggyupi <beryl32451@gmail.com>
2023-10-03 09:01:07 +0700

    modifikasi percobaan pertama nomer 4

--- Pemilihan2Percobaan105.java
+++ Pemilihan2Percobaan105.java
@@ -9,10 +9,13 @@ public class Pemilihan2Percobaan105 {
         System.out.print("Masukkan tahun : ");
         int tahun = input05.nextInt();
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
-                System.out.println("Tahun Kabisat");   
+            if ((tahun % 100) == 0) {
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else
+                System.out.println("Bukan Tahun Kabisat");   
             } else
-                System.out.println("Bukan Tahun Kabisat");                 
+                System.out.println("Tahun Kabisat");                 
         } else 
             System.out.println("Bukan Tahun Kabisat");
         input05.close();
