922a5b1870dbf62ab173fc1e5126c20f74a92313
ggyupi <beryl32451@gmail.com>
2023-10-03 14:07:05 +0700

    modifikasi percobaan 3

--- Pemilihan2Percobaan305.java
+++ Pemilihan2Percobaan305.java
@@ -15,7 +15,7 @@ public class Pemilihan2Percobaan305 {
         System.out.print("Masukkan banyaknya penghasilan : ");
         penghasilan = input05.nextInt();
 
-        if ((kategori.equalsIgnoreCase("pekerja"))) {
+        if ((kategori.equals("pekerja"))) {
             if (penghasilan <= 2000000)
                 pajak = 0.1;
             else if (penghasilan <= 3000000)
@@ -24,7 +24,7 @@ public class Pemilihan2Percobaan305 {
                 pajak = 0.2;
                 gajiBersih = (int) (penghasilan - (pajak * penghasilan));
                 System.out.print("Penghasilan bersih : " + gajiBersih);
-        } else if ((kategori.equalsIgnoreCase("pebisnis"))) {
+        } else if ((kategori.equals("pebisnis"))) {
             if (penghasilan <= 2500000)
                 pajak = 0.15;
             else if (penghasilan <= 3500000)
